2710c5d521ba4edbf4117891a67bd282c3e00eb2
d3d777 <andre.soares13@fatec.sp.gov.br>
2024-03-24 14:31:35 -0300

    adding id search on the api

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -4,25 +4,31 @@ import com.algaworks.awpag.domain.model.Cliente;
 import com.algaworks.awpag.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 
-    private ClienteRepository clienteRepository;
-
-    public ClienteController(ClienteRepository clienteRepository) {
-        this.clienteRepository = clienteRepository;
-    }
+    private final ClienteRepository clienteRepository;
 
     @GetMapping("/clientes")
     public List<Cliente> listar(){
         return clienteRepository.findByNomeContaining("Joao");
     }
 
+    @GetMapping("/clientes/{clientId}")
+    public Cliente buscar(@PathVariable Long clientId){
+        Optional<Cliente> cliente = clienteRepository.findById(clientId);
+
+        return cliente.orElse(null);
+    }
 }
